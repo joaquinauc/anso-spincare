9d07c1c8a344f4595130605bdc957d635efab902
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-26 12:24:40 -0600

    Add class to user info labels

--- app/static/js/add_user.js
+++ app/static/js/add_user.js
@@ -3,13 +3,14 @@ const editButton = document.querySelector('.edit-button');
 const closeModalButton = document.querySelector('.close');
 const addUserModal = document.querySelector('.modal-user');
 
-const USER_INFO_LABELS = ['Nombres:', 'Apellido Paterno:', 'Apellido Materno (Opcional):', 'Contraseña:', 'Rol'];
+const USER_INFO_LABELS = ['Nombres:', 'Apellido Paterno:', 'Apellido Materno:', 'Contraseña:', 'Rol:'];
 const USER_INFO_DATA_ID = ['user-show-name', 'user-show-first-last-name', 'user-show-second-last-name', 'user-show-password', 'user-show-role']
 
 for (let i = 0; i < USER_INFO_DATA_ID.length; i++) {
     const userInfoSubContainer = document.createElement('div');
 
     const userInfoLabel = document.createElement('label');
+    userInfoLabel.setAttribute('class', 'user-info-label');
     userInfoLabel.textContent = USER_INFO_LABELS[i];
 
     const userInfoData = document.createElement('p');
